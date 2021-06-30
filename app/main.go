962b4677d8f8bb1e55b5514448b340490243e8dd
omoterikuto <omoterikuto@gmail.com>
2021-07-01 00:30:39 +0900

    json化

--- app/main.go
+++ app/main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"database/sql"
 	"log"
+	"encoding/json"
 	// "net/http"
 	// "time"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type Recipe struct {
-	ID          int
-	Title       string
-	CreatedAt   string
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	MakingTime  string `json:"making_time"`
+	Serves		  string `json:"serves"`
+	Ingredients string `json:"ingredients"`
+	Cost        string `json:"cost"`
 }
+
+type OutPutAll struct {
+	Recipes []Recipe
+}
+
 const (
 	// DriverName ドライバ名(mysql固定)
 	DriverName = "mysql"
@@ -24,15 +33,38 @@ const (
 var rcp = make(map[int]Recipe)
 
 func main() {
+	
+	getAll()
+}
+
+func getAll() {
 	db, dbErr := sql.Open(DriverName, DataSourceName)
 	if dbErr != nil {
 		log.Print("error connecting to database:", dbErr)
 	}
 	defer db.Close()
-	rows, queryErr := db.Query("SELECT * FROM recipes")
+	rows, queryErr := db.Query("SELECT id, title, making_time, serves, ingredients, cost FROM recipes")
 	if queryErr != nil {
 			log.Print("query error :", queryErr)
 	}
 	defer rows.Close()
-	fmt.Println(rows)
+
+	var outPut OutPutAll
+	
+	for rows.Next() {
+		var recipe Recipe
+
+    if err := rows.Scan(&recipe.Id, &recipe.Title, &recipe.MakingTime, &recipe.Serves, &recipe.Ingredients, &recipe.Cost); err != nil {
+      log.Fatal(err)
+    }
+
+		outPut.Recipes = append(outPut.Recipes, Recipe{recipe.Id, recipe.Title, recipe.MakingTime, recipe.Serves, recipe.Ingredients, recipe.Cost})
+	}
+
+	outPutJson, err := json.Marshal(outPut)
+	if err != nil {
+		fmt.Printf("json変換エラー", err)
+	}
+
+	fmt.Println(string(outPutJson))
 }
